e76488d1248520ba8980cc8dd20e1e865a9b12af
RektPunk <rektpunk@gmail.com>
2024-09-03 13:02:17 +0900

    update common and remove jupyter

--- cookbook/views/jupyter.py
+++ cookbook/views/jupyter.py
@@ -1,5 +1,4 @@
 import json
-import os
 import re
 from typing import Dict, List, Optional
 
@@ -7,12 +6,6 @@ import black
 import isort
 import reflex as rx
 import requests
-
-from cookbook.templates import template
-
-BASE_RAW_PATH = os.path.join(
-    os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "notebooks"
-)  # FIXME: input basic raw path including url
 
 
 def _is_url(path) -> bool:
@@ -117,20 +110,3 @@ def jupyter(path: str, image_base_url: Optional[str] = None) -> rx.Component:
         *[_style_cell(cell=cell, image_base_url=image_base_url) for cell in _cells],
         width="100%",
     )
-
-
-def create_route_component(route: str, file_path: str, **kwargs) -> rx.Component:
-    @template(route=route, **kwargs)
-    def dynamic_component() -> rx.Component:
-        _file_path = os.path.join(BASE_RAW_PATH, file_path)
-        if file_path.endswith(".ipynb"):
-            return jupyter(path=_file_path)
-
-        if file_path.endswith(".md"):
-            with open(_file_path, encoding="utf-8") as file:
-                content = file.read()
-            return rx.markdown(content)
-
-        return rx.markdown("")
-
-    return dynamic_component
